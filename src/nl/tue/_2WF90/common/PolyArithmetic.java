2d17410f4fb359d251dac33304949628a57c5ee4
Dodecahedra <k.degeling@student.tue.nl>
2018-10-09 15:46:20 +0200

    Wrote contracts for addition subtraction

---
+++ src/nl/tue/_2WF90/common/PolyArithmetic.java
@@ -0,0 +1,37 @@
+package nl.tue._2WF90.common;
+
+/**
+ * Class for the Polynomial arithmetic operations
+ *
+ * @author E.M.A. Arts (1004076)
+ * @author K. Degeling (1018025)
+ * @author R.M. Jonker (1011291)
+ * @author S. Jacobs (1005276)
+ * @author M. Schotsman (0995661)
+ *
+ * @since 27 SEPTEMBER 2018
+ */
+public class PolyArithmetic {
+    /**
+     * Polynomial addition (mod p)
+     * @param a Polynomial a
+     * @param b Polynomial b
+     * @param p modulo p, with p a prime, p < (bound)
+     * @return a+b(mod p)
+     */
+    public Polynomial polyAdd(Polynomial a, Polynomial b, int p) {
+
+    }
+
+    /**
+     * Polynomial subtraction (mod p)
+     * @param a Polynomial a
+     * @param b Polynomial b
+     * @param p modulo p, with p a prime, p < (bound)
+     * @return a-b(mod p)
+     */
+    public Polynomial polySubtract(Polynomial a, Polynomial b, int p) {
+
+    }
+
+}
