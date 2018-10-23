c31101c49958bcb5b55debe74e16c2e672b215f5
Dodecahedra <k.degeling@student.tue.nl>
2018-10-23 14:36:07 +0200

    Final implementation Finite Field Arithmetic.

--- src/nl/tue/_2WF90/common/FiniteFieldArithmetic.java
+++ src/nl/tue/_2WF90/common/FiniteFieldArithmetic.java
@@ -40,6 +40,34 @@ public class FiniteFieldArithmetic {
     public static Polynomial add(Polynomial a, Polynomial b, FiniteField f) {
         return Division.modulo(PolyArithmetic.polyAdd(a, b, f.getMod()), f.getModPoly(), f.getMod());
     }
+    /**
+     * Finite Field subtraction (mod p)
+     * @param c
+     * @return c.f - c.g (mod c.mod)
+     */
+    public static Polynomial subtract(Computation c) {
+        return add(c.getF(), c.getG(), new FiniteField(c));
+    }
+
+    /**
+     * Finite Field subtraction
+     * @param a Polynomial a
+     * @param b Polynomial b
+     * @param f Finite Field f
+     * @return a-b(mod f)
+     */
+    public static Polynomial subtract(Polynomial a, Polynomial b, FiniteField f) {
+        return Division.modulo(PolyArithmetic.polySubtract(a, b, f.getMod()), f.getModPoly(), f.getMod());
+    }
+
+    /**
+     * Finite Field multiplication (mod p)
+     * @param c
+     * @return c.f * c.g (mod c.mod)
+     */
+    public static Polynomial multiply(Computation c) {
+        return multiply(c.getF(), c.getG(), new FiniteField(c));
+    }
 
     /**
      * Finite Field Multiplication (mod p)
@@ -52,9 +80,6 @@ public class FiniteFieldArithmetic {
         return Division.modulo(PolyMultiplication.polyMultiply(a, b, f.getMod()), f.getModPoly(), f.getMod());
     }
 
-    public static Polynomial subtract(Polynomial a, Polynomial b, FiniteField f) {
-        return Division.modulo(PolyArithmetic.polySubtract(a, b, f.getMod()), f.getModPoly(), f.getMod());
-    }
 
 
 }
